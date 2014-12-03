85b5729fafaad08a102b157238ff3835a6372a3d
naoyashiga <naoyashiga0@gmail.com>
2014-12-03 19:40:50 +0900

    【add】現在時刻取得

--- Thirty/ViewController.swift
+++ Thirty/ViewController.swift
@@ -17,6 +17,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let date = NSDate() // -> "Nov 2, 2014, 10:32 AM"
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.CalendarUnitYear | .CalendarUnitMonth | .CalendarUnitDay | .CalendarUnitHour | .CalendarUnitMinute | .CalendarUnitSecond, fromDate: date)
+        
+        let year = components.year
+        let hour = components.hour // -> 10
+        let minutes = components.minute // -> 32
+        let second = components.second // -> 22
+        
+        println(components)
     }
 
     override func didReceiveMemoryWarning() {
