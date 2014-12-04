8a79af14d44ec0672af2c93544bbe4fd272452b0
naoyashiga <naoyashiga0@gmail.com>
2014-12-04 09:04:03 +0900

    【add】ボタンを押すとバウンドする

--- Thirty/ViewController.swift
+++ Thirty/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var hours: UILabel!
     @IBOutlet weak var minutes: UILabel!
     @IBOutlet weak var seconds: UILabel!
+    var myView:UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +40,38 @@ class ViewController: UIViewController {
         println(components)
         
         println(birthday)
+        
+
+        // myViewを生成.
+        myView = UIView(frame: CGRectMake(0, 0, 100, 100))
+        myView.backgroundColor = UIColor.orangeColor()
+        myView.layer.position = CGPointMake(self.view.frame.width/2, self.view.frame.height/2)
+        
+        self.view.addSubview(myView)
+        
+    }
+    
+    func scaleView(view: UIView, magnitude: CGFloat) {
+        let anime = POPSpringAnimation()
+//        anime.property = POPAnimatableProperty.propertyWithName(kPOPViewScaleXY) as POPAnimatableProperty
+        anime.property = POPAnimatableProperty.propertyWithName(kPOPLayerSize) as POPAnimatableProperty
+        anime.springBounciness = 12.0
+        anime.springSpeed = 10.0
+        
+//        let newRect = CGRect(x: (view.bounds.origin.x + (view.bounds.size.width / 2.0)) - ((view.bounds.size.width / 2.0) * magnitude), y: (view.bounds.origin.y + (view.bounds.size.height / 2.0)) - ((view.bounds.size.height / 2.0) * magnitude), width: view.bounds.size.width * magnitude, height: view.bounds.size.height * magnitude)
+        
+//        let newRect = CGRect(x: 0, y: 0, width: 120, height: 100)
+//        anime.fromValue = NSValue(CGRect: view.bounds)
+//        anime.toValue = NSValue(CGRect: newRect)
+        anime.toValue = NSValue(CGSize:CGSizeMake(150, 150))
+        view.pop_addAnimation(anime, forKey: "bound")
+        
+        
+    }
+    
+    @IBAction func sampleBtn(sender: UIButton) {
+        println("click")
+        scaleView(myView, magnitude: 0.005)
     }
 
     override func didReceiveMemoryWarning() {
