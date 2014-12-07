87648109689288bb59205cd49e61bcdb31cf227f
naoyashiga <naoyashiga0@gmail.com>
2014-12-07 20:27:49 +0900

    【add】pod追加

--- Podfile
+++ Podfile
@@ -1,1 +1,3 @@
 pod 'pop', '~> 1.0'
+pod 'EAIntroView', '~> 2.6.0'
+pod 'WSCoachMarksView', '~> 0.2'
--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -6,6 +6,18 @@
 // project.
 
 
+// EAIntroView
+#define COCOAPODS_POD_AVAILABLE_EAIntroView
+#define COCOAPODS_VERSION_MAJOR_EAIntroView 2
+#define COCOAPODS_VERSION_MINOR_EAIntroView 6
+#define COCOAPODS_VERSION_PATCH_EAIntroView 4
+
+// WSCoachMarksView
+#define COCOAPODS_POD_AVAILABLE_WSCoachMarksView
+#define COCOAPODS_VERSION_MAJOR_WSCoachMarksView 0
+#define COCOAPODS_VERSION_MINOR_WSCoachMarksView 2
+#define COCOAPODS_VERSION_PATCH_WSCoachMarksView 0
+
 // pop
 #define COCOAPODS_POD_AVAILABLE_pop
 #define COCOAPODS_VERSION_MAJOR_pop 1
